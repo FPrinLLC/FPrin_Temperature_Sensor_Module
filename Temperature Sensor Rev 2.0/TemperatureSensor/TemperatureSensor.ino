336bd4bab8329fc43d5aefebb8c084e3aab3083f
ljparrish <125088225+ljparrish@users.noreply.github.com>
2023-10-28 01:45:38 -0700

    Updated to work with nRF52840
    Updated to work with Adafruit nRF52840 feather dev board. Now uses Bluefruit BLE library

--- Temperature Sensor Rev 2.0/TemperatureSensor/TemperatureSensor.ino
+++ Temperature Sensor Rev 2.0/TemperatureSensor/TemperatureSensor.ino
@@ -1,27 +1,20 @@
-#include <Adafruit_MAX1704X.h>
-#include <ArduinoBLE.h>
+#include <Adafruit_NeoPixel.h>
+#include <bluefruit.h>
 #include <Wire.h>
+#include "IEEE11073float.h"
 
-
-BLEService BLETemperatureSensor("181A");
-
-// Bluetooth® Low Energy Battery Level Characteristics
-BLEFloatCharacteristic Temperature("2A6E",  // standard 16-bit characteristic UUID
-                                   BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
-
+BLEService BLETemperatureSensor = BLEService(0x181A);
+BLECharacteristic Temperature = BLECharacteristic(0x2A6E);
+BLEDis bledis;
+                              
 int SENSOR_ADDRESS = 0x48;
-int oldBatteryLevel = 0;  // last battery level reading from analog input
-
-float previousTemperature = 0;
-float measuredTemperature = 0;
-
-unsigned long currentMillis, previousMillis;
+uint8_t tempData[2];
+float measuredTemperature = 0.0;
 
 void setup() {
   byte error;
   Wire.begin();
   Serial.begin(115200);
-  while (!Serial);
 
   Serial.println("BLE Temperature Sensor Module v1.01");
 
@@ -38,63 +31,35 @@ void setup() {
     while (1);
   }
   Serial.println("Temperature Sensor Initialization successful!");
-
-
-  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
-
   // begin initialization
-  if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
+  
+  Bluefruit.begin();
+  Bluefruit.setName("Temperature Sensor");
 
-    while (1);
-  }
+  bledis.setModel("Temperature Sensor Module");
+  bledis.setManufacturer("FPrin LLC");
+  bledis.begin();
 
-  /* Set a local name for the Bluetooth® Low Energy device
-     This name will appear in advertising packets
-     and can be used by remote devices to identify this Bluetooth® Low Energy device
-     The name can be changed but maybe be truncated based on space left in advertisement packet
-  */
-  BLE.setLocalName("Temperature Sensor");
-  BLE.setAdvertisedService(BLETemperatureSensor); // add the service UUID
-  BLETemperatureSensor.addCharacteristic(Temperature); // add the battery level characteristic
-  BLE.addService(BLETemperatureSensor); // Add the battery service
-  Temperature.writeValue(previousTemperature); // set initial value for this characteristic
-
-  /* Start advertising Bluetooth® Low Energy.  It will start continuously transmitting Bluetooth® Low Energy
-     advertising packets and will be visible to remote Bluetooth® Low Energy central devices
-     until it receives a new connection */
-
-  // start advertising
-  BLE.advertise();
+  setupTempSensorBLE();
+  
+  Bluefruit.Advertising.addService(BLETemperatureSensor);
+  Bluefruit.Advertising.addName();
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.start(0);
 
   Serial.println("Bluetooth® device active, waiting for connections...");
-  previousMillis = millis();
+  digitalWrite(LED_BLUE, LOW);
 }
 
 void loop() {
-  // wait for a Bluetooth® Low Energy central
-  BLEDevice central = BLE.central();
 
   // if a central is connected to the peripheral:
-  if (central) {
-    Serial.print("Connected to central: ");
-    // print the central's BT address:
-    Serial.println(central.address());
-    // turn on the LED to indicate the connection:
-    digitalWrite(LED_BUILTIN, HIGH);
-
+  if (Bluefruit.connected()) {
     // update temperature value every second
     // while the central is connected:
-    while (central.connected()) {
-      measuredTemperature = measureTemperature(SENSOR_ADDRESS);
-      Temperature.writeValue(measuredTemperature);
-      printTemperature();
-      delay(1000);
-    }
-    // when the central disconnects, turn off the LED:
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.print("Disconnected from central: ");
-    Serial.println(central.address());
+    measuredTemperature = measureTemperature(SENSOR_ADDRESS);
+    Temperature.notify(tempData,sizeof(tempData));
+    printTemperature();
   }
   delay(1000);
 }
@@ -104,14 +69,16 @@ float measureTemperature(int address) {
   Wire.requestFrom(address, 2);
   byte buff[2];
   Wire.readBytes(buff, 2);
-  //Serial.print(buff[0],BIN);
+  //Serial.print(buff[0],DEC);
   //Serial.print("  ");
-  //Serial.println(buff[1],BIN);
+  //Serial.println(buff[1],DEC);
   if (bitRead(buff[0], 7) == 1) { // If the first bit is a 1
     Temperature = (~((buff[0] << 8) + buff[1]) + 1) * -0.0078125; // Compute Negative Temperature
   } else { // If the first bit is a 0
     Temperature = ((buff[0] << 8) + buff[1]) * 0.0078125; // Compute Positive Temperature
   }
+  tempData[0] = buff[0];
+  tempData[1] = buff[1];
   return Temperature;
 }
 
@@ -138,4 +105,16 @@ void readConfigRegister(int address) {
   Wire.endTransmission();
   Serial.println(buff[0], BIN);
   Serial.println(buff[1], BIN);
+}
+
+void setupTempSensorBLE() {
+  // Start Temperature Sensor Service
+  BLETemperatureSensor.begin();
+
+  // Setup Temperature Measurement Characteristic
+  Temperature.setProperties(CHR_PROPS_NOTIFY);
+  Temperature.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  Temperature.setFixedLen(2);
+  Temperature.begin();
+  Temperature.write(tempData,sizeof(tempData));
 }
